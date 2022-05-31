aaf792f4b8315d3819de90417338b91cf0d626e9
tnichols <tnichols@student.dalat.org>
2022-05-31 22:27:25 +0800

    move config

--- bootloader.nix
+++ bootloader.nix
@@ -1,5 +1,7 @@
 { config, pkgs, ... }:
 {
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.consoleMode = "max";
+  boot.loader.systemd-boot.editor = false;
   boot.loader.efi.canTouchEfiVariables = true;
 }
--- users/users.nix
+++ users/users.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-  # home-manager = pkgs.home-manager;
 in
 {
   imports =
