79cc440809b2d14a07b66cc2850bd2e39e56e37e
tnichols <tnichols@student.dalat.org>
2022-06-01 02:04:51 +0800

    move config

--- users/tev.nix
+++ users/tev.nix
@@ -5,6 +5,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     hashedPassword = ''$6$HeYdqp57.G0nM9K/$.i5eB/rANAYEeGYLxdvbM8jklRy3UebqHsWoHMabC5yQaUoaday9xhbCS7CVF9jANq0isTJK8US7C5/WNmPP7.'';
+    shell = pkgs.fish;
   };
 
   home-manager.users.tev = {
