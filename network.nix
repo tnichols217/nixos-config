c7324840ca05084532a8ee558f756e4668696bb9
tnichols <tnichols@student.dalat.org>
2022-05-31 21:53:43 +0800

    move config

--- network.nix
+++ network.nix
@@ -3,4 +3,9 @@
   networking.networkmanager.enable = true;
   networking.useDHCP = false;
   networking.interfaces.enp1s0.useDHCP = true;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 }
