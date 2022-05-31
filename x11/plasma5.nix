a865c6ba6099b684b3bf71303896ef9b8004d259
tnichols <tnichols@student.dalat.org>
2022-05-31 22:43:44 +0800

    move config

--- x11/plasma5.nix
+++ x11/plasma5.nix
@@ -1,5 +1,12 @@
 { config, pkgs, ... }:
 {
   services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.enableHidpi = true;
+  services.xserver.displayManager.sddm.theme = "${(pkgs.fetchFromGitHub {
+    owner = "MarianArlt";
+    repo = "kde-plasma-chili";
+    rev = "a371123959676f608f01421398f7400a2f01ae06";
+    sha256 = "17pkxpk4lfgm14yfwg6rw6zrkdpxilzv90s48s2hsicgl3vmyr3x";
+})}";
   services.xserver.desktopManager.plasma5.enable = true;
 }
