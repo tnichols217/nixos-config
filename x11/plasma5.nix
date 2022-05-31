ffa6e66097bdae974002cbc644d0acd96aef48da
tnichols <tnichols@student.dalat.org>
2022-05-31 22:55:08 +0800

    move config

--- x11/plasma5.nix
+++ x11/plasma5.nix
@@ -5,7 +5,7 @@
   services.xserver.displayManager.sddm.theme = "${(pkgs.fetchFromGitHub {
     owner = "MarianArlt";
     repo = "kde-plasma-chili";
-    # rev = "a371123959676f608f01421398f7400a2f01ae06";
+    rev = "latest";
     # sha256 = "17pkxpk4lfgm14yfwg6rw6zrkdpxilzv90s48s2hsicgl3vmyr3x";
 })}";
 #   services.xserver.displayManager.sddm.theme = "${builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz"}";
