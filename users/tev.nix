4d93dfdce3eafefa1eda3acfb079fb098e1585d2
tnichols <tnichols@student.dalat.org>
2022-05-31 21:34:48 +0800

    move config

--- users/tev.nix
+++ users/tev.nix
@@ -1,12 +1,5 @@
 { config, pkgs, ... }:
-let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-in
 {
-  imports =
-    [
-      (import "${home-manager}/nixos")
-    ];
 
   users.users.tev = {
     isNormalUser = true;
