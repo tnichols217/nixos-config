8fd814a731ac486811f2041b02520aa5718e9a7f
tnichols <tnichols@student.dalat.org>
2022-06-01 00:00:16 +0800

    move config

--- users/tev.nix
+++ users/tev.nix
@@ -13,7 +13,6 @@
         vscodium
         obsidian
         barrier
-        firefox
         gnupg
       ];
     };
@@ -23,6 +22,38 @@
         userName = "tnichols217";
         userEmail = "62992267+tnichols217@users.noreply.github.com";
       };
+      fish = {
+        enable = true;
+        plugins = [
+          {
+            name = "fzf";
+            src = pkgs.fetchFromGitHub {
+              owner = "jethrokuan";
+              repo = "fzf";
+              rev = "ac01d96fc6344ebeb48c03f2c9c0be5bf3b20f1c";
+              sha256 = "1h97zh3ghcvvn2x9rj51frhhi85nf7wa072g9mm2pc6sg71ijw4k";
+            };
+          }
+        ];
+        shellAliases = {
+          clip = "xclip -selection clipboard -r";
+          gac = "git add -A && git commit -am ";
+          gp = "git push ";
+          gpl = "git pull ";
+        };
+      };
+    };
+    firefox = {
+      enable = true;
+      package = nur.repos.wolfangaukang.librewolf-bin;
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        https-everywhere
+        privacy-badger
+        darkreader
+        ublock-origin
+        sponsorblock
+        videospeed
+      ];
     };
   };
 }
