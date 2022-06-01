eef254c7a3824597dcc01ce1d92b5c3b34d3efa5
tnichols <tnichols@student.dalat.org>
2022-06-02 00:32:27 +0800

    move config

--- configuration/users/tev.nix
+++ configuration/users/tev.nix
@@ -1,16 +1,16 @@
 { config, pkgs, ... }:
-# let
-#   username = tev;
-# in
+let
+  username = tev;
+in
 {
-  users.users.tev = {
+  users.users.${username} = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     hashedPassword = ''$6$HeYdqp57.G0nM9K/$.i5eB/rANAYEeGYLxdvbM8jklRy3UebqHsWoHMabC5yQaUoaday9xhbCS7CVF9jANq0isTJK8US7C5/WNmPP7.'';
     shell = pkgs.fish;
   };
 
-  home-manager.users.tev = {
+  home-manager.users.${username} = {
     home = {
       stateVersion = "21.11";
       packages = with pkgs; [
@@ -102,7 +102,7 @@
 
   environment.persistence."/nix/persist/etc/nixos/configuration/persistence/data" = {
     hideMounts = false;
-    users.tev = {
+    users.${username} = {
       directories = [
 
         "Downloads"
