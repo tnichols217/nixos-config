5938e41f466a8563758edbdd25ae62252d1c2e56
tnichols <tnichols@student.dalat.org>
2022-06-01 20:10:52 +0800

    move config

--- configuration/users.nix
+++ configuration/users.nix
@@ -1,11 +1,10 @@
-{ config, pkgs, impermanence, ... }:
+{ config, pkgs, ... }:
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
 in
 {
   imports =
     [
-      (import "${impermanence}/home-manager.nix")
       (import "${home-manager}/nixos")
       ./users/tev.nix
     ];
