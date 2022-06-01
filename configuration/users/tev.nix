3b18d54f342ea94a0addc91aaa2bfbb42c7803f5
tnichols <tnichols@student.dalat.org>
2022-06-01 20:32:09 +0800

    move config

--- configuration/users/tev.nix
+++ configuration/users/tev.nix
@@ -97,25 +97,25 @@
     };
   };
 
-  environment.persistence."/persistent" = {
-    hideMounts = false;
-    users.talyz = {
-      directories = [
-        "Downloads"
-        "Music"
-        "Pictures"
-        "Documents"
-        "Videos"
-        "VirtualBox VMs"
-        { directory = ".gnupg"; mode = "0700"; }
-        { directory = ".ssh"; mode = "0700"; }
-        { directory = ".nixops"; mode = "0700"; }
-        { directory = ".local/share/keyrings"; mode = "0700"; }
-        ".local/share/direnv"
-      ];
-      files = [
-        ".screenrc"
-      ];
-    };
-  };
+  # environment.persistence."/persistent" = {
+  #   hideMounts = false;
+  #   users.talyz = {
+  #     directories = [
+  #       "Downloads"
+  #       "Music"
+  #       "Pictures"
+  #       "Documents"
+  #       "Videos"
+  #       "VirtualBox VMs"
+  #       { directory = ".gnupg"; mode = "0700"; }
+  #       { directory = ".ssh"; mode = "0700"; }
+  #       { directory = ".nixops"; mode = "0700"; }
+  #       { directory = ".local/share/keyrings"; mode = "0700"; }
+  #       ".local/share/direnv"
+  #     ];
+  #     files = [
+  #       ".screenrc"
+  #     ];
+  #   };
+  # };
 }
