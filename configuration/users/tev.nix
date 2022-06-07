317ab80b387b1fe0686b324a0305df7f576cc837
tnichols <tnichols@student.dalat.org>
2022-06-07 18:33:42 +0800

    move config

--- configuration/users/tev.nix
+++ configuration/users/tev.nix
@@ -6,7 +6,7 @@ in
   imports =
   [
     ((import ./templates/persist.nix) { username = "${username}"; })
-    "./${username}/arch-theme.nix"
+    ./tev/arch-theme.nix
   ];
 
   users.users.${username} = {
