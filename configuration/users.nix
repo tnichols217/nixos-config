908b26e0cc529bff1973e4b8fa41c82aeb9d15bc
tnichols <tnichols@student.dalat.org>
2022-06-10 01:57:12 +0800

    move config

--- configuration/users.nix
+++ configuration/users.nix
@@ -1,12 +1,12 @@
 { config, pkgs, impermanence, ... }:
 let
   # home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/20703892473d01c70fb10248442231fe94f4ceb4.tar.gz";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/1808cb66aa2134d9e627d4aa868ee95e5fe683ed.tar.gz";
 in
 {
   imports =
     [
-      # (import "${home-manager}/nixos")
+      (import "${home-manager}/nixos")
       ./users/tev.nix
     ];
   users.mutableUsers = false;
