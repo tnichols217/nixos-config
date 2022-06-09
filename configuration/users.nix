f431c199054c1018171d1a97319aaab0790e5a9c
tnichols <tnichols@student.dalat.org>
2022-06-10 01:06:22 +0800

    move config

--- configuration/users.nix
+++ configuration/users.nix
@@ -1,12 +1,10 @@
 { config, pkgs, impermanence, ... }:
 let
-  # impermanence = builtins.fetchTarball "https://github.com/nix-community/impermanence/archive/master.tar.gz";
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
 in
 {
   imports =
     [
-      # (import "${impermanence}/nixos.nix")
       (import "${home-manager}/nixos")
       ./users/tev.nix
     ];
--- configuration/users/tev/plasma_config/export.py
+++ configuration/users/tev/plasma_config/export.py
@@ -3,7 +3,6 @@
 import json
 import os
 
-CUR = os.getcwd()
 with open("./config.json") as f:
     j = json.load(f)
 
@@ -15,7 +14,7 @@ for i in j:
     for k in j[i]:
         for l in k[1]:
             if (len(l) == 2):
-                o += "kwriteconfig5 --file \"" + CUR + "/conf" + san(i)  + "\" --group \"" + "\" --group \"".join([san(i) for i in k[0]]) + "\" --key \"" + san(l[0]) + "\" \"" + san(l[1]) + "\"\n"
+                o += "kwriteconfig5 --file \"" + "./conf" + san(i)  + "\" --group \"" + "\" --group \"".join([san(i) for i in k[0]]) + "\" --key \"" + san(l[0]) + "\" \"" + san(l[1]) + "\"\n"
 
 with open("./config.sh", "w") as f:
     f.write(o)
