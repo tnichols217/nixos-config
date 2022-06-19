13fd27086f56e75c79ec45c5892ea1e2ea97da00
tnichols <tnichols@student.dalat.org>
2022-06-19 21:14:25 +0800

    move config

--- flake.nix
+++ flake.nix
@@ -2,9 +2,18 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.home-manager.url = github:nix-community/home-manager;
   inputs.nur.url = github:nix-community/NUR;
-  inputs.omf.url = github:oh-my-fish/theme-bobthefish;
-  inputs.arch-theme.url = github:rkstrdee/Arch;
-  inputs.papirus.url = github:PapirusDevelopmentTeam/papirus-icon-theme;
+  inputs.omf = {
+    url = github:oh-my-fish/theme-bobthefish;
+    flake = false;
+  };
+  inputs.arch-theme = {
+    url = github:rkstrdee/Arch;
+    flake = false;
+  };
+  inputs.papirus = {
+    url = github:PapirusDevelopmentTeam/papirus-icon-theme;
+    flake = false;
+  };
   
   outputs = { self, nixpkgs, ... }@attrs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
