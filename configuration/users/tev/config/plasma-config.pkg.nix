2b93debf009ed33b21f6f9537923db6335287db4
tnichols <tnichols@student.dalat.org>
2022-06-24 02:25:42 +0800

    move config

--- configuration/users/tev/config/plasma-config.pkg.nix
+++ configuration/users/tev/config/plasma-config.pkg.nix
@@ -1,4 +1,4 @@
-{ pkgs, attrs, ... }:
+{ pkgs, ... }:
 pkgs.callPackage ../metapkgs/combine.metapkg.nix { pack = [
   ( pkgs.callPackage ./plasma-config/autostart.pkg.nix {} )
   ( pkgs.callPackage ./plasma-config/rc.pkg.nix {} )
