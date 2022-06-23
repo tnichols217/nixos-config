d5f46e43428f54b50a24994f7a325736a0e414b4
tnichols <tnichols@student.dalat.org>
2022-06-24 01:35:22 +0800

    move config

--- configuration/users/tev/packages.nix
+++ configuration/users/tev/packages.nix
@@ -15,8 +15,8 @@
         python3
         spotify
         # config.nur.repos.wolfangaukang.stremio
-        # (callPackage ./packages/arch-theme.pkg.nix { inherit arch-theme; })
-        # (callPackage ./packages/papirus-icons.pkg.nix { inherit papirus; })
+        (callPackage ./packages/arch-theme.pkg.nix { arch-theme = attrs.arch-theme; })
+        (callPackage ./packages/papirus-icons.pkg.nix { papirus = attrs.papirus; })
         (callPackage ./packages/kwin-scripts.pkg.nix {})
         (callPackage ./packages/konsole-themes.pkg.nix {})
       ];
--- configuration/x11/plasma5.nix
+++ configuration/x11/plasma5.nix
@@ -1,7 +1,7 @@
-{ config, pkgs, chili, ... }:
+{ config, pkgs, attrs, ... }:
 {
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.sddm.enableHidpi = true;
-  services.xserver.displayManager.sddm.theme = "${chili}";
+  services.xserver.displayManager.sddm.theme = "${attrs.chili}";
   services.xserver.desktopManager.plasma5.enable = true;
 }
