0d5386f5eef13415518f26c167139623a536019e
tnichols <tnichols@student.dalat.org>
2022-06-25 22:30:04 +0800

    move config

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
       flake = false;
     };
     ublock = {
-      url = "file+https://addons.mozilla.org/firefox/downloads/file/3961087/ublock_origin-1.43.0.xpi";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3961087/ublock_origin-1.43.0.xpi";
       flake = false;
     };
   };
