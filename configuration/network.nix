a783735b73a32a0848f49e48f45a015e44678817
tnichols <tnichols@student.dalat.org>
2022-06-27 00:12:49 +0800

    move config

--- configuration/network.nix
+++ configuration/network.nix
@@ -2,7 +2,7 @@
 {
   networking.networkmanager.enable = true;
   networking.useDHCP = false;
-  networking.hostName = hostname
+  networking.hostName = hostname;
   # networking.interfaces.enp1s0.useDHCP = true;
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
