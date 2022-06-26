a000c971c8b5595d71ea14bb1f0ab0fdedc5c6fb
tnichols <tnichols@student.dalat.org>
2022-06-27 00:20:24 +0800

    move config

--- configuration/users/tev/config/plasma-config/rc/config.nix
+++ configuration/users/tev/config/plasma-config/rc/config.nix
@@ -1502,7 +1502,7 @@ in
         "autoConfigPrompted"= "true";
         "autoHide"= "true";
         "autoStart"= "false";
-        "configFile"= "${./config/barrier/ + hostname + ".conf"}";
+        "configFile"= "${./config/barrier + hostname + ".conf"}";
         "cryptoEnabled"= "true";
         "elevateMode"= "false";
         "elevateModeEnum"= "0";
