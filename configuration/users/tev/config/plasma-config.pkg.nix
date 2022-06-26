c9b4aac0bdde065a393d1e7a52e0ba2bf1c25c39
tnichols <tnichols@student.dalat.org>
2022-06-27 00:27:31 +0800

    move config

--- configuration/users/tev/config/plasma-config.pkg.nix
+++ configuration/users/tev/config/plasma-config.pkg.nix
@@ -1,5 +1,5 @@
-{ pkgs, ... }:
+{ pkgs, host-name, ... }:
 pkgs.callPackage ../metapkgs/combine.metapkg.nix { pack = [
   ( pkgs.callPackage ./plasma-config/autostart.pkg.nix {} )
-  ( pkgs.callPackage ./plasma-config/rc.pkg.nix {} )
+  ( pkgs.callPackage ./plasma-config/rc.pkg.nix { inherit host-name; } )
 ]; }
