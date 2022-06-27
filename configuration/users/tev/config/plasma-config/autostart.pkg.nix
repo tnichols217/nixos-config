281ecd18da88f9647956a2abaf09e6f65852523d
tnichols <tnichols@student.dalat.org>
2022-06-28 04:24:12 +0800

    move config

--- configuration/users/tev/config/plasma-config/autostart.pkg.nix
+++ configuration/users/tev/config/plasma-config/autostart.pkg.nix
@@ -3,7 +3,7 @@ pkgs.callPackage ../../metapkgs/combine.metapkg.nix { pack = [
   (pkgs.callPackage ./autostart/save-desktop.pkg.nix { app = pkgs.flameshot; name = "org.flameshot.Flameshot"; })
   (pkgs.callPackage ./autostart/save-desktop.pkg.nix { app = pkgs.barrier; name = "barrier"; })
   (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { pkg = pkgs.ibus; name = "ibus-daemon"; })
-  (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { exec = ""; name = "bash"; args = "${pkgs.writeScriptBin "setWallpaper" ''while true; do qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = \"org.kde.image\";d.currentConfigGroup = Array(\"Wallpaper\", \"org.kde.image\", \"General\");d.writeConfig(\"Image\", \"file://${import ./rc/config/background.nix}\")}'; sleep 10; done''}"; })
+  (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { exec = ""; name = "bash"; args = "${pkgs.writeScriptBin "setWallpaper" ''while true; do qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file://${import ./rc/config/background.nix}")}'; sleep 10; done''}" + "/bin/setWallpaper"; })
 ]; }
 
 
