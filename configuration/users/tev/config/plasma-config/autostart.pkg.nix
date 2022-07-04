6b4ec4128967bbb2b25ccba5d055d7112fa95190
tnichols <tnichols@student.dalat.org>
2022-07-04 17:43:03 +0800

    move config

--- configuration/users/tev/config/plasma-config/autostart.pkg.nix
+++ configuration/users/tev/config/plasma-config/autostart.pkg.nix
@@ -3,7 +3,23 @@ pkgs.callPackage ../../metapkgs/combine.metapkg.nix { pack = [
   (pkgs.callPackage ./autostart/save-desktop.pkg.nix { app = pkgs.flameshot; name = "org.flameshot.Flameshot"; })
   (pkgs.callPackage ./autostart/save-desktop.pkg.nix { app = pkgs.barrier; name = "barrier"; })
   (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { pkg = pkgs.ibus; name = "ibus-daemon"; })
-  (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { exec = ""; name = "bash"; args = "${pkgs.writeScriptBin "setWallpaper" ''while true; do qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file://${import ./rc/config/background.nix}")}'; sleep 10; done''}" + "/bin/setWallpaper"; })
+  (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { exec = ""; name = "bash"; args = let 
+    plugin = "org.kde.image";
+    wp = import ./rc/config/background.nix;
+  in "${pkgs.writeScriptBin "setWallpaper" ''
+  qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+    var allDesktops = desktops()
+    var alreadySet = false
+    while (!alreadySet) {
+        alreadySet = allDesktops.map((d) => {
+            d.currentConfigGroup = Array("Wallpaper", "${plugin}", "General")
+            return d.readConfig("Image") == "file://${wp}"
+        }).every(a => a)
+        print(alreadySet)
+        allDesktops.forEach((d) => {
+            d.wallpaperPlugin = "${plugin}";
+            d.currentConfigGroup = Array("Wallpaper", "${plugin}", "General");
+            d.writeConfig("Image", "file://${wp}")
+        })
+    }';''}" + "/bin/setWallpaper"; })
 ]; }
-
-
