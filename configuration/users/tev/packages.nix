7ae33d603b9da106afb0095ce6ef56e4e4c3d38f
tnichols <tnichols@student.dalat.org>
2022-07-05 13:39:26 +0800

    move config

--- configuration/users/tev/packages.nix
+++ configuration/users/tev/packages.nix
@@ -65,7 +65,9 @@
           "..." = "cd ../..";
           "...." = "cd ../../..";
           cdg = "cd $(git rev-parse --show-toplevel)";
+          upt  = "cd /etc/nixos; sudo nix flake update";
           upg = "sudo bash -c \"cd /etc/nixos; git pull; nixos-rebuild boot --flake \\\".#${host-name}\\\"\"";
+          uptg = "upt; upg";
           sci = "ssh-copy-id -i ~/.ssh/ed25519 ";
         };
         plugins = [
