3aa55e37453b996d84b84fb984bff73b41ac81d4
tnichols <tnichols@student.dalat.org>
2022-07-10 00:56:37 +0800

    move config

--- configuration/users/tev/config/plasma-config/rc/config/background.nix
+++ configuration/users/tev/config/plasma-config/rc/config/background.nix
@@ -1,4 +1,4 @@
 { host-name }:
-"$(if host-name == "ASUS" then ./background/Anime-Street-Night.png else (
+"${if host-name == "ASUS" then ./background/Anime-Street-Night.png else (
   if host-name == "MSI" then ./background/Anime-Street-Evening.png else ./background/Anime-Street-Day.png
-))"
+)}"
