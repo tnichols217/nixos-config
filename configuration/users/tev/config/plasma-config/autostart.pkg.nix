20d172a6701751c15691bf9790c308abeff4db7f
tnichols <tnichols@student.dalat.org>
2022-07-10 01:00:26 +0800

    move config

--- configuration/users/tev/config/plasma-config/autostart.pkg.nix
+++ configuration/users/tev/config/plasma-config/autostart.pkg.nix
@@ -6,7 +6,7 @@ pkgs.callPackage ../../metapkgs/combine.metapkg.nix { pack = [
   (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { exec = ""; name = "bash"; args = let
     algo = "ed25519";
     plugin = "org.kde.image";
-    wp = import ./rc/config/background.nix { inherit host-name; };
+    wp = (import ./rc/config/background.nix { inherit host-name; });
   in "${pkgs.writeScriptBin "setWallpaper" ''
     if [ ! -f ~/.ssh/${algo} ]; then
       ssh-keygen -t ${algo} -f ~/.ssh/${algo} -N ""
