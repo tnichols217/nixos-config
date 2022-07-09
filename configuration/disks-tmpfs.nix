cc20a75d2bb455b615247cda0c479b39834480aa
tnichols <tnichols@student.dalat.org>
2022-07-09 23:17:36 +0800

    move config

--- configuration/disks-tmpfs.nix
+++ configuration/disks-tmpfs.nix
@@ -14,7 +14,7 @@
     "/" = {
       device = "none";
       fsType = "tmpfs";
-      options = [ "defaults" "size=4G" "mode=755" ];
+      options = [ "defaults" "size=8G" "mode=755" ];
     };
 
     "/nix" = {
