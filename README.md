9f7610a0a94555259e8dc99380a52c8d98341809
tnichols <tnichols@student.dalat.org>
2022-07-10 00:52:33 +0800

    move config

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ First Partition two disks:
 1. One for Boot in format FAT with label NIXBOOT `mkfs.fat -F 32 DISK; fatlabel DISK NIXBOOT`
 2. One for Nix in format ext4 with label NIXROOT `mkfs.ext4 DISK -L NIXROOT`
 3. One for Persistence in format ext4 with label NIXPERSIST `mkfs.ext4 DISK -L NIXPERSIST`
-4. One for Userdata in format ext4 with label NIXDATA `mkfs.ext4 DISK -L NIXDATA`
-5. One for Large, non-important data in format ext4 with label NIXBUCKET `mkfs.ext4 DISK -L NIXBUCKET`
-5. One for swap in format linux-swap with label NIXSWAP `mkswap DISK -L NIXBUCKET`
+4. One for swap in format linux-swap with label NIXSWAP `mkswap DISK -L NIXSWAP`
+5. One for Userdata in format ext4 with label NIXDATA `mkfs.ext4 DISK -L NIXDATA`
+6. One for Large, non-important data in format ext4 with label NIXBUCKET `mkfs.ext4 DISK -L NIXBUCKET`
 
 Install with: `curl https://raw.githubusercontent.com/tnichols217/nixos-config/main/install | sudo sh -s `_modulename_
--- configuration/disks-tmpfs.nix
+++ configuration/disks-tmpfs.nix
@@ -16,36 +16,42 @@
       device = "none";
       fsType = "tmpfs";
       options = [ "defaults" "size=8G" "mode=755" ];
+      neededForBoot = true;
     };
 
     # software
     "/nix" = {
       device = "/dev/disk/by-label/NIXROOT";
       fsType = "ext4";
+      neededForBoot = true;
     };
 
     # boot
     "/boot" = {
       device = "/dev/disk/by-label/NIXBOOT";
       fsType = "vfat";
+      neededForBoot = true;
     };
 
     # local data
     "/nix/persist" = {
       device = "/dev/disk/by-label/NIXPERSIST";
       fsType = "ext4";
+      neededForBoot = true;
     };
 
     # data
     "/nix/persist/etc/nixos/configuration/persistence/data/home" = {
       device = "/dev/disk/by-label/NIXDATA";
       fsType = "ext4";
+      neededForBoot = true;
     };
 
     # bucket
     "/nix/persist/etc/nixos/configuration/persistence/bucket/home" = {
       device = "/dev/disk/by-label/NIXBUCKET";
       fsType = "ext4";
+      neededForBoot = true;
     };
   };
 
