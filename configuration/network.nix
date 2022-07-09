d7fa9db3499cc117f8241cba0fe2520771bcdefd
tnichols <tnichols@student.dalat.org>
2022-07-09 15:53:32 +0800

    move config

--- configuration/network.nix
+++ configuration/network.nix
@@ -1,17 +1,17 @@
 { config, pkgs, host-name, ... }:
 {
-  # networking.networkmanager.enable = true;
+  networking.networkmanager.enable = true;
   networking.useDHCP = false;
   networking.hostName = host-name;
-  networking.wireless = {
-    enable = true;
-    userControlled.enable = true;
-    networks = {
-      "NICHOLS 5Ghz" = {
-        psk = "0194815801";
-      };
-    };
-  };
+  # networking.wireless = {
+  #   enable = true;
+  #   userControlled.enable = true;
+  #   networks = {
+  #     "NICHOLS 5Ghz" = {
+  #       psk = "0194815801";
+  #     };
+  #   };
+  # };
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # networking.firewall.enable = false;
