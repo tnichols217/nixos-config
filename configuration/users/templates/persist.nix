4bc97d366aa45ff2f2bec346b8054fcc8be6b73b
tnichols <tnichols@student.dalat.org>
2022-07-09 18:10:20 +0800

    move config

--- configuration/users/templates/persist.nix
+++ configuration/users/templates/persist.nix
@@ -1,40 +1,41 @@
 { config, pkgs, username, ... }:
 {
-  environment.persistence."/nix/persist/etc/nixos/configuration/persistence/data" = let 
-  mapDir = ( x: { directory = x; mode = "0700"; user = "${username}"; } );
-  in {
-    hideMounts = false;
-    users.${username} = {
-      directories = pkgs.lib.lists.map mapDir [
-        "Downloads"
-        "Desktop"
-        "Music"
-        "Pictures"
-        "Documents"
-        "Videos"
-      ];
+  environment.persistence = let 
+      mapDir = ( x: { directory = x; mode = "0700"; user = "${username}"; } );
+    in {
+    "/nix/persist/etc/nixos/configuration/persistence/data" = {
+      hideMounts = false;
+      users.${username} = {
+        directories = pkgs.lib.lists.map mapDir [
+          "Downloads"
+          "Desktop"
+          "Music"
+          "Pictures"
+          "Documents"
+          "Videos"
+        ];
+      };
     };
-  };
-
-  environment.persistence."/nix/persist/etc/nixos/configuration/persistence/local" = {
-    hideMounts = false;
-    users.${username} = {
-      directories = pkgs.lib.lists.map mapDir [
-        ".ssh"
-        ".gnupg"
-        ".nixops"
-        ".steam"
-        ".mozilla"
-        ".local/share/barrier"
-        ".local/share/Steam"
-        ".local/share/osu"
-        ".local/share/TelegramDesktop"
-        ".local/share/kwalletd"
-        ".local/share/kscreen"
-        ".config/discordptb"
-        ".config/libvirt"
-        ".config/spotify"
-      ];
+    "/nix/persist/etc/nixos/configuration/persistence/local" = {
+      hideMounts = false;
+      users.${username} = {
+        directories = pkgs.lib.lists.map mapDir [
+          ".ssh"
+          ".gnupg"
+          ".nixops"
+          ".steam"
+          ".mozilla"
+          ".local/share/barrier"
+          ".local/share/Steam"
+          ".local/share/osu"
+          ".local/share/TelegramDesktop"
+          ".local/share/kwalletd"
+          ".local/share/kscreen"
+          ".config/discordptb"
+          ".config/libvirt"
+          ".config/spotify"
+        ];
+      };
     };
   };
 }
