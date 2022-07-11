3368e7475085572761cca435425012d671370ce9
tnichols <tnichols@student.dalat.org>
2022-07-11 16:45:52 +0800

    move config

--- configuration/localization.nix
+++ configuration/localization.nix
@@ -1,7 +1,12 @@
 { config, pkgs, ... }:
 {
   time.timeZone = "Asia/Kuala_Lumpur";
-  i18n.defaultLocale = "en_SG.UTF-8";
+  i18n = {
+    defaultLocale = "en_SG.UTF-8";
+    inputMethod = {
+      ibus.engines = with pkgs.ibus-engines; [ libpinyin typing-booster uniemoji ];
+    };
+  };
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
