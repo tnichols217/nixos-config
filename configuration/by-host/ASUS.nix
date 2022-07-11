08f072666a05d918581b8718bfdf0a6f1af7678c
tnichols <tnichols@student.dalat.org>
2022-07-11 23:14:18 +0800

    move config

--- configuration/by-host/ASUS.nix
+++ configuration/by-host/ASUS.nix
@@ -39,6 +39,6 @@
 
   services.cron = {
     enable = true;
-    systemCronJobs = [ ''0,5,10,15,20,25,30,35,40,45,50,55 * * * * wwwrun sh /storage/church/config/update.sh'' ];
+    systemCronJobs = [ ''0,5,10,15,20,25,30,35,40,45,50,55 * * * * root sh /storage/church/config/update.sh'' ];
   };
 }
