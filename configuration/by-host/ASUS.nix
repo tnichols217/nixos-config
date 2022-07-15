f0895856bf8b39db822453e370f73abb36c909e2
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-07-15 23:26:46 +0800

    move config

--- configuration/by-host/ASUS.nix
+++ configuration/by-host/ASUS.nix
@@ -86,13 +86,13 @@
           { name = "libpipewire-module-adapter"; }
           { name = "libpipewire-module-link-factory"; }
           { name = "libpipewire-module-session-manager"; }
-          {
-            name = "libpipewire-module-pulse-tunnel";
-            args = {
-              tunnel.mode = "sink";
-              pulse.server.address = "tcp:192.168.100.250";
-            };
-          }
+          # {
+          #   name = "libpipewire-module-pulse-tunnel";
+          #   args = {
+          #     tunnel.mode = "sink";
+          #     pulse.server.address = "tcp:192.168.100.250";
+          #   };
+          # }
         ];
       };
     };
