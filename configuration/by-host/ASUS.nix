11abc545b235264404155cbf60e789105bc3bb29
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-07-15 23:21:25 +0800

    move config

--- configuration/by-host/ASUS.nix
+++ configuration/by-host/ASUS.nix
@@ -59,39 +59,39 @@
       #     }
       #   ];
       # };
-      pipewire = {
-        "context.modules" = [
-          {
-            name = "libpipewire-module-rt";
-            args = {
-              nice.level = -11;
-            };
-            flags = [ "ifexists" "nofail" ];
-          }
-          { name = "libpipewire-module-protocol-native"; }
-          { name = "libpipewire-module-profiler"; }
-          { name = "libpipewire-module-metadata"; }
-          { name = "libpipewire-module-spa-device-factory"; }
-          { name = "libpipewire-module-spa-node-factory"; }
-          { name = "libpipewire-module-client-node" ;}
-          { name = "libpipewire-module-client-device"; }
-          {
-            name = "libpipewire-module-portal";
-            flags = [ "ifexists" "nofail" ];
-          }
-          { name = "libpipewire-module-access"; }
-          { name = "libpipewire-module-adapter"; }
-          { name = "libpipewire-module-link-factory"; }
-          { name = "libpipewire-module-session-manager"; }
-          {
-            name = "libpipewire-module-pulse-tunnel";
-            args = {
-              tunnel.mode = "sink";
-              pulse.server.address = "tcp:192.168.100.250";
-            };
-          }
-        ];
-      };
+      # pipewire = {
+      #   "context.modules" = [
+      #     {
+      #       name = "libpipewire-module-rt";
+      #       args = {
+      #         nice.level = -11;
+      #       };
+      #       flags = [ "ifexists" "nofail" ];
+      #     }
+      #     { name = "libpipewire-module-protocol-native"; }
+      #     { name = "libpipewire-module-profiler"; }
+      #     { name = "libpipewire-module-metadata"; }
+      #     { name = "libpipewire-module-spa-device-factory"; }
+      #     { name = "libpipewire-module-spa-node-factory"; }
+      #     { name = "libpipewire-module-client-node" ;}
+      #     { name = "libpipewire-module-client-device"; }
+      #     {
+      #       name = "libpipewire-module-portal";
+      #       flags = [ "ifexists" "nofail" ];
+      #     }
+      #     { name = "libpipewire-module-access"; }
+      #     { name = "libpipewire-module-adapter"; }
+      #     { name = "libpipewire-module-link-factory"; }
+      #     { name = "libpipewire-module-session-manager"; }
+      #     {
+      #       name = "libpipewire-module-pulse-tunnel";
+      #       args = {
+      #         tunnel.mode = "sink";
+      #         pulse.server.address = "tcp:192.168.100.250";
+      #       };
+      #     }
+      #   ];
+      # };
     };
   };
 }
