ae22d80f3b80172d230136ced798afee521e1914
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-07-16 23:06:26 +0800

    move config

--- configuration/persistence/local.nix
+++ configuration/persistence/local.nix
@@ -9,6 +9,7 @@
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
       "/etc/letsencrypt"
+      "/etc/ssh"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
     files = [
