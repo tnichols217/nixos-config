840ff96d0ff28595860200da2d7e326f8e9a85f8
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-07-16 15:29:54 +0800

    move config

--- configuration/network.nix
+++ configuration/network.nix
@@ -1,6 +1,6 @@
 { config, pkgs, host-name, ... }:
 {
-  networking.useDHCP = false;
+  # networking.useDHCP = false;
   networking.hostName = host-name;
   hardware.enableRedistributableFirmware = true;
   networking.firewall.enable = false;
