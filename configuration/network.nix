a08d22663fb6f1da5a42e3c90e7e3b4714ad03a4
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-08-10 09:56:05 +0800

    fix np servers and remove auto set vsocde color theme

--- configuration/network.nix
+++ configuration/network.nix
@@ -3,8 +3,7 @@
   networking.hostName = host-name;
   hardware.enableRedistributableFirmware = true;
   networking.firewall.enable = false;
-  networking.timeServers = {
-    [
+  networking.timeServers = [
       "0.nixos.pool.ntp.org"
       "1.nixos.pool.ntp.org"
       "2.nixos.pool.ntp.org"
@@ -13,6 +12,5 @@
       "time2.google.com"
       "time3.google.com"
       "time4.google.com"
-    ]
-  };
+  ];
 }
--- configuration/users/tev/packages/vscode.nix
+++ configuration/users/tev/packages/vscode.nix
@@ -10,10 +10,11 @@
     "editor.fontLigatures" = true;
     "editor.guides.bracketPairs" = true;
     "editor.bracketPairColorization.enabled" = true;
-    "window.autoDetectColorScheme" = true;
+    # "window.autoDetectColorScheme" = true;
     "editor.semanticHighlighting.enabled" = true;
     "workbench.colorTheme" = "Default Dark+";
     "workbench.iconTheme" = "vscode-icons";
+
   };
   mutableExtensionsDir = false;
   extensions = pkgs.lib.lists.forEach (builtins.attrNames (builtins.readDir (attrs.program-extensions + "/vscode/out"))) (x: 
