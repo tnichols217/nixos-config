e5aa2f1e71789f7b37d16adba8cf8aa27c14d9c1
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-08-10 08:33:09 +0800

    add google timeservers because nixos's are blocked

--- configuration/network.nix
+++ configuration/network.nix
@@ -3,4 +3,16 @@
   networking.hostName = host-name;
   hardware.enableRedistributableFirmware = true;
   networking.firewall.enable = false;
+  networking.timeServers = {
+    [
+      "0.nixos.pool.ntp.org"
+      "1.nixos.pool.ntp.org"
+      "2.nixos.pool.ntp.org"
+      "3.nixos.pool.ntp.org"
+      "time.google.com"
+      "time2.google.com"
+      "time3.google.com"
+      "time4.google.com"
+    ]
+  };
 }
