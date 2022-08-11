7f4639edef4b92c1ee9bec6434aaa91e4176c789
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-08-11 10:43:57 +0800

    remove auto color scheme from vscode

--- configuration/users/tev/packages/vscode.nix
+++ configuration/users/tev/packages/vscode.nix
@@ -10,7 +10,6 @@
     "editor.fontLigatures" = true;
     "editor.guides.bracketPairs" = true;
     "editor.bracketPairColorization.enabled" = true;
-    # "window.autoDetectColorScheme" = true;
     "editor.semanticHighlighting.enabled" = true;
     "workbench.colorTheme" = "Default Dark+";
     "workbench.iconTheme" = "vscode-icons";
