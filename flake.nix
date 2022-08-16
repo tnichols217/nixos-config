56838c69921c89ce6212c46ce02e7cff10b6b696
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-08-17 00:34:48 +0800

    experiment with iso

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
     nur = {
       url = github:nix-community/NUR;
     };
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     btf = {
       url = github:oh-my-fish/theme-bobthefish;
       flake = false;
@@ -43,24 +47,31 @@
   };
   
   outputs = { self, nixpkgs, ... }@attrs: let 
-    mods = [
-        attrs.home-manager.nixosModules.default
-        attrs.impermanence.nixosModules.impermanence
-        attrs.nur.nixosModules.nur
-        ./configuration.nix
-      ];
-    version = "21.11";
-    fullAttrs = { inherit attrs version; };
+      mods = [
+          attrs.home-manager.nixosModules.default
+          attrs.impermanence.nixosModules.impermanence
+          attrs.nur.nixosModules.nur
+          ./configuration.nix
+        ];
+      version = "21.11";
+      fullAttrs = { inherit attrs version; };
     in {
-    nixosConfigurations.MSI = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = fullAttrs // { host-name = "MSI"; };
-      modules = mods;
+    nixosConfigurations = {
+      MSI = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = fullAttrs // { host-name = "MSI"; };
+        modules = mods;
+      };
+      ASUS = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = fullAttrs // { host-name = "ASUS"; };
+        modules = mods;
+      };
     };
-    nixosConfigurations.ASUS = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = fullAttrs // { host-name = "ASUS"; };
-      modules = mods;
+    packages.x86_64-linux = {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = mods;
+        format = "iso";
     };
   };
 }
