1ffe31ad626b7dac3ba26207c06507165b1d51fc
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-09-07 09:28:51 +0800

    nvidia offload polymc

--- configuration/users/tev/config/plasma-config/autostart.pkg.nix
+++ configuration/users/tev/config/plasma-config/autostart.pkg.nix
@@ -15,6 +15,9 @@ pkgs.callPackage ../../metapkgs/combine.metapkg.nix { pack = [
     if [ ! -f ~/.local/share/applications/steam.desktop ]; then
       sed 's/^Exec=/&nvidia-offload /' /run/current-system/sw/share/applications/steam.desktop > ~/.local/share/applications/steam.desktop
     fi
+    if [ ! -f ~/.local/share/applications/org.polymc.PolyMC.desktop ]; then
+      sed 's/^Exec=/&nvidia-offload /' /run/current-system/sw/share/applications/org.polymc.PolyMC.desktop > ~/.local/share/applications/org.polymc.PolyMC.desktop
+    fi
     '' else ""}
     while true;
     do qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
