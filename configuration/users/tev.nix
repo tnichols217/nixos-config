a8147bbbf635f3ee65c2f4fe9d5f2e39d8aecb8f
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-09-18 02:12:23 +0800

    syntax

--- configuration/users/tev.nix
+++ configuration/users/tev.nix
@@ -7,7 +7,7 @@ in
   [
     ((import ./templates/persist.nix) (args // { inherit username; }))
     ((import ./templates/normal_sudo.nix) (args // { inherit username; }))
-    ((import ./templates/default_home_manager.nix) (args // { inherit pkgs username host-name version; btf = attrs.btf }))
+    ((import ./templates/default_home_manager.nix) (args // { inherit pkgs username host-name version; btf = attrs.btf; }))
     ((import ./tev/config.nix) (args // { inherit username; }))
     ((import ./tev/packages.nix) (args // { inherit username arch-theme papirus vscodeExtensions; }))
   ];
