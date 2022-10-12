5afeeb256429e4d9c22e4e849ae1f9d91bdd1179
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-10-12 22:29:26 +0800

    add remote builders

--- configuration/by-host/MSI.nix
+++ configuration/by-host/MSI.nix
@@ -38,6 +38,6 @@
 
   nix.settings = {
     "max-jobs" = 0;
-    "builders" = "ssh://tev@pigsgo.mooo.com?ssh-key=/home/tev/.ssh/ed25519 x86_64-linux 10 - - kvm";
+    "builders" = "ssh://tev@pigsgo.mooo.com?ssh-key=/home/tev/.ssh/ed25519 x86_64-linux - 8 4 kvm - c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUExd1hDelZSTkt5VHlLUENVYUZBV0xwaFRRdmt1citpUDZMWEszYWRWY0sgcm9vdEBBU1VTCg==";
   };
 }
