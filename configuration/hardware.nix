d0fbbba8e8fc7de87c52e7026e798f2291a9b5c3
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-12-23 12:36:48 +0800

    add ntfs

--- configuration/hardware.nix
+++ configuration/hardware.nix
@@ -13,6 +13,7 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [
     v4l2loopback
   ];
+  boot.supportedFilesystems = [ "ntfs" ];
   powerManagement.cpuFreqGovernor = "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.video.hidpi.enable = true;
