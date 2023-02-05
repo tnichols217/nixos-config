0e375f7c9f1c8175d63850bd234042754f0f5740
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-02-05 22:41:21 +0800

    try fix vscode

--- configuration/users/tev/packages/vscode.nix
+++ configuration/users/tev/packages/vscode.nix
@@ -2,24 +2,8 @@
 
 {
   enable = true;
-  package = pkgs.vscode-with-extensions.override {
-    # vscode = pkgs.vscodium;
-    vscodeExtensions = pkgs.lib.lists.forEach (builtins.attrNames (builtins.readDir (attrs.program-extensions.packages."x86_64-linux".default + "/vscode"))) (x: 
-      let 
-        vsix = attrs.program-extensions.packages."x86_64-linux".default + "/vscode/" + x;
-        name = pkgs.lib.strings.splitString "." x;
-      in
-      pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-        vsix = vsix;
-        mktplcRef = {
-          publisher = builtins.elemAt name 0;
-          name = builtins.elemAt name 1;
-          pname = builtins.elemAt name 1;
-          version = "latest";
-        };
-      }
-    );
-  };
+  package = pkgs.vscodium
+
   userSettings = {
     "[nix]"."editor.tabSize" = 2;
     "editor.renderWhitespace" = "all";
