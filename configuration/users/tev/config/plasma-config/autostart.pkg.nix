ad9f361697001cd5c3b8659aa5cb0fbb3b4cc57a
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-02-06 00:04:50 +0800

    fix bash autostart

--- configuration/users/tev/config/plasma-config/autostart.pkg.nix
+++ configuration/users/tev/config/plasma-config/autostart.pkg.nix
@@ -3,7 +3,7 @@ pkgs.callPackage ../../metapkgs/combine.metapkg.nix { pack = [
   (pkgs.callPackage ./autostart/save-desktop.pkg.nix { app = pkgs.flameshot; name = "org.flameshot.Flameshot"; })
   (pkgs.callPackage ./autostart/save-desktop.pkg.nix { app = pkgs.barrier; name = "barrier"; })
   (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { pkg = pkgs.cachix; name = "cachix"; args = "watch-store tnichols217-nixos-config"; })
-  (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { exec = ""; name = "bash"; args = let
+  (pkgs.callPackage ./autostart/custom-autostart.pkg.nix { pkg = pkgs.bash; name = "bash"; args = let
     algo = "ed25519";
     plugin = "org.kde.image";
     wp = (import ./rc/config/background.nix { inherit host-name; });
