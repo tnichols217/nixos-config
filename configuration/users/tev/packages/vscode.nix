826136fb8831c06abfa7e6fd7b4c59dfc7a7805c
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-05-01 16:53:59 +0800

    remove vscode exts from xonfig

--- configuration/users/tev/packages/vscode.nix
+++ configuration/users/tev/packages/vscode.nix
@@ -3,20 +3,22 @@
   enable = true;
   package = with vscode_exts; (pkgs.vscode-with-extensions.override {
     vscode = pkgs.vscodium;
-    vscodeExtensions = pkgs.lib.lists.forEach (builtins.attrNames (builtins.readDir (attrs.program-extensions.packages."x86_64-linux".default + "/vscode"))) (x: 
-      let 
-        vsix = attrs.program-extensions.packages."x86_64-linux".default + "/vscode/" + x;
-        name = pkgs.lib.strings.splitString "." x;
-      in
-      pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-        vsix = vsix;
-        mktplcRef = {
-          publisher = builtins.elemAt name 0;
-          name = builtins.elemAt name 1;
-          version = "latest";
-        };
-      }
-    ) ++ [
+    vscodeExtensions = 
+    # pkgs.lib.lists.forEach (builtins.attrNames (builtins.readDir (attrs.program-extensions.packages."x86_64-linux".default + "/vscode"))) (x: 
+    #   let 
+    #     vsix = attrs.program-extensions.packages."x86_64-linux".default + "/vscode/" + x;
+    #     name = pkgs.lib.strings.splitString "." x;
+    #   in
+    #   pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+    #     vsix = vsix;
+    #     mktplcRef = {
+    #       publisher = builtins.elemAt name 0;
+    #       name = builtins.elemAt name 1;
+    #       version = "latest";
+    #     };
+    #   }
+    # ) ++ 
+    [
       alefragnani.project-manager
       bbenoist.QML
       codezombiech.gitignore
