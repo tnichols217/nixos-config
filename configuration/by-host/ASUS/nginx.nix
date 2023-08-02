2fa3e0126e98bb32aab4d68f8110dd890a0faa48
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-08-03 01:15:29 +0800

    rewrite redis config

--- configuration/by-host/ASUS/nginx.nix
+++ configuration/by-host/ASUS/nginx.nix
@@ -9,9 +9,10 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     virtualHosts = {
-      "pigsgo.mooo.com" = {
+      "nix-serve" = {
         locations."/".proxyPass = "http://${config.services.nix-serve.bindAddress}:${toString config.services.nix-serve.port}";
         useACMEHost = "pigsgo.mooo.com";
+        serverName = "pigsgo.mooo.com";
         forceSSL = true;
         # sslCertificateKey = "/var/lib/acme/pigsgo.mooo.com/key.pem";
         # sslCertificate = "/var/lib/acme/pigsgo.mooo.com/cert.pem";
@@ -19,6 +20,19 @@
           {
             addr = "0.0.0.0";
             port = 5000;
+            ssl = true;
+          }
+        ];
+      };
+      "nextcloud" = {
+        locations."/".proxyPass = "http://localhost:80";
+        useACMEHost = "pigsgo.mooo.com";
+        serverName = "pigsgo.mooo.com";
+        forceSSL = true;
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 443;
             ssl = true;
           }
         ];
