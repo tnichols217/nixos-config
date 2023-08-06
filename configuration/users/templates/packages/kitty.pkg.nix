33ab33566a8e362738b7b3387db84ccf27e794d4
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-08-06 23:22:42 +0800

    mod kitty package

--- configuration/users/templates/packages/kitty.pkg.nix
+++ configuration/users/templates/packages/kitty.pkg.nix
@@ -1,2 +1,28 @@
-{ pkgs, attrs, ... }:
-pkgs.writeShellScriptBin "kitty" "${pkgs.kitty}/bin/kitty -c ~/.config/kitty/kitty.conf -c $(ls ${"${attrs.kitty-themes}/themes"} | shuf -n 1) $argv"
+{ pkgs ? import <nixpkgs> {}, attr, ... }:
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "dot-config";
+  version = "v1.0.0";
+
+  src = pkgs.kitty;
+
+  installPhase =''
+
+  mkdir $out
+
+  cp -r * $out
+
+  cd $out/bin
+
+  mv kitty kitty-unwrapped
+  cp ${pkgs.writeShellScriptBin "kitty" "${pkgs.kitty}/bin/kitty -c ~/.config/kitty/kitty.conf -c $(ls ${"${attrs.kitty-themes}/themes"} | shuf -n 1) $argv"}/bin/* .
+
+  '';
+
+  meta = {
+    description = "my kitty randomizer";
+    homepage = "https://github.com/tnichols217/nixos-config";
+    license = pkgs.lib.licenses.gpl3;
+    platforms = pkgs.lib.platforms.all;
+  };
+}
