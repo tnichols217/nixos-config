13091e46afe5faa1111994e0761ca0a42afa6ae5
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-10-02 12:53:29 -0400

    update microcode for amd

--- configuration/hardware.nix
+++ configuration/hardware.nix
@@ -16,7 +16,7 @@
   boot.supportedFilesystems = [ "ntfs" ];
   powerManagement.cpuFreqGovernor = "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.amd.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.bluetooth.enable = true;
   services.ratbagd.enable = true;
   services.xserver.libinput.mouse.scrollMethod = "button";
