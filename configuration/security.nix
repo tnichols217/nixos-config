b72e1d9ffb8945efabf09e9d99e2f7ee8bff26e0
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-10-16 18:11:14 -0400

    persist yubikey auth

--- configuration/security.nix
+++ configuration/security.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 {
+  services.udev.packages = [ pkgs.yubikey-personalization ];
   programs.gnupg.agent = {
     enable = true;
     pinentryFlavor = "qt";
+    enableSSHSupport = true;
   };
 }
