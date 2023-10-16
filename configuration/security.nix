9b80cd547d1908293e1f58edd1f6a618329908fe
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-10-16 18:38:31 -0400

    enable more p2f

--- configuration/security.nix
+++ configuration/security.nix
@@ -7,10 +7,14 @@
     enableSSHSupport = true;
   };
 
+  security.pam.u2f.enable = true;
+
   security.pam.services = {
     sddm.u2fAuth = true;
+    sddm-greeter.u2fAuth = true;
     login.u2fAuth = true;
     sudo.u2fAuth = true;
     kde.u2fAuth = true;
+    xscreensaver.u2fAuth = true;
   };
 }
