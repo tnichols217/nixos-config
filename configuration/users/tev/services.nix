23ba84f1056b65fe70ac143626d0730f31fac1e2
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-10-24 20:45:14 -0400

    fix sunshine (?)

--- configuration/users/tev/services.nix
+++ configuration/users/tev/services.nix
@@ -1,17 +1,17 @@
 { pkgs, attrs, config, username, ... }:
 {
-  systemd.services."${username}@sunshine" = {
-    serviceConfig.Type = "simple";
-    path = with pkgs; [ pkgs.sunshine ];
-    serviceConfig = {
-      ExecStart = "${pkgs.sunshine}/bin/sunshine";
-      Restart = "on-failure";
-    };
-    serviceConfig = {
-      Environment = "\"HOME=/home/${username}\" XDG_CONFIG_HOME=/home/${username}/.config";
-    };
-    wantedBy = ["multi-user.target"];
-  };
+  # systemd.services."${username}@sunshine" = {
+  #   serviceConfig.Type = "simple";
+  #   path = with pkgs; [ pkgs.sunshine ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.sunshine}/bin/sunshine";
+  #     Restart = "on-failure";
+  #   };
+  #   serviceConfig = {
+  #     Environment = "\"HOME=/home/${username}\" XDG_CONFIG_HOME=/home/${username}/.config";
+  #   };
+  #   wantedBy = ["multi-user.target"];
+  # };
   systemd.services."${username}@cachix" = {
     serviceConfig.Type = "simple";
     path = with pkgs; [ pkgs.cachix ];
