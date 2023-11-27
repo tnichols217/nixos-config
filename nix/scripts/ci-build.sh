43eaa0c173a1a9852cf0ae3c49ce5b39cc1c8ca8
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-11-27 06:06:47 -0500

    add keep going to cibuild

--- nix/scripts/ci-build.sh
+++ nix/scripts/ci-build.sh
@@ -1,3 +1,3 @@
-nix build .#nixosConfigurations.ASUS.config.system.build.toplevel
-nix build .#nixosConfigurations.MSI.config.system.build.toplevel
-nix build .#nixosConfigurations.ROG.config.system.build.toplevel
+nix build --keep-going .#nixosConfigurations.ASUS.config.system.build.toplevel
+nix build --keep-going .#nixosConfigurations.MSI.config.system.build.toplevel
+nix build --keep-going .#nixosConfigurations.ROG.config.system.build.toplevel
