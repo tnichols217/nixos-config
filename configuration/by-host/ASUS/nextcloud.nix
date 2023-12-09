55fbc8e2bff9c74a537f82f55afb21e6220cf19c
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2023-12-09 03:20:47 -0500

    fix prostgresql

--- configuration/by-host/ASUS/nextcloud.nix
+++ configuration/by-host/ASUS/nextcloud.nix
@@ -7,7 +7,7 @@
     ensureUsers = [
       {
         name = "nextcloud";
-        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
+        ensureDBOwnership = true;
       }
     ];
   };
