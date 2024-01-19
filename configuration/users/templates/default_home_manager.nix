ef4458ee222611d38a176ede300f55508033398e
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-01-19 09:04:03 -0500

    try again

--- configuration/users/templates/default_home_manager.nix
+++ configuration/users/templates/default_home_manager.nix
@@ -61,7 +61,7 @@
         };
       };
       nix-index.enable = true;
-      command-not-found.enable = pkgs.lib.mkForce false;
+      # command-not-found.enable = false;
       fish = {
         enable = true;
         shellAliases = {
