ea246e634ee2be69c5d01ac07dab2a67c8d9ef9e
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-01-19 21:05:59 -0500

    undo pipewire conf

--- configuration/by-host/ASUS/pipewire.nix
+++ configuration/by-host/ASUS/pipewire.nix
@@ -2,36 +2,36 @@
 {
   # TODO move pipewire config to config files instead
 
-  services.pipewire = {
-    extraConfig = {
-      pipewire = {
-        "10-network-sink" = {
-          "context.modules" = [
-            {
-              name = "libpipewire-module-pulse-tunnel";
-              args = {
-                "tunnel.mode" = "sink";
-                "pulse.server.address" = "tcp:tln32rog.student.cwru.edu";
-              };
-            }
-            {
-              name = "libpipewire-module-zeroconf-discover";
-              args = {};
-            }
-          ];
-        };
-      };
+  # services.pipewire = {
+  #   extraConfig = {
+  #     pipewire = {
+  #       "10-network-sink" = {
+  #         "context.modules" = [
+  #           {
+  #             name = "libpipewire-module-pulse-tunnel";
+  #             args = {
+  #               "tunnel.mode" = "sink";
+  #               "pulse.server.address" = "tcp:tln32rog.student.cwru.edu";
+  #             };
+  #           }
+  #           {
+  #             name = "libpipewire-module-zeroconf-discover";
+  #             args = {};
+  #           }
+  #         ];
+  #       };
+  #     };
 
-      pipewire-pulse = {
-        "10-network-sink" = {
-          "pulse.properties" = {
-            "server.address" = [
-              "unix:native"
-              "tcp:4713"
-            ];
-          };
-        };
-      };
-    };
-  };
+  #     pipewire-pulse = {
+  #       "10-network-sink" = {
+  #         "pulse.properties" = {
+  #           "server.address" = [
+  #             "unix:native"
+  #             "tcp:4713"
+  #           ];
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 }
