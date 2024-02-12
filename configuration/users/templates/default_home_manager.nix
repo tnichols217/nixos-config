177f973599c148d3a28a17d171b348d81d4490f1
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-02-12 13:53:40 -0500

    syntax

--- configuration/users/templates/default_home_manager.nix
+++ configuration/users/templates/default_home_manager.nix
@@ -50,5 +50,11 @@
         };
       };
     };
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = ["qemu:///system"];
+        uris = ["qemu:///system"];
+      };
+    };
   };
 }
--- configuration/virtualization.nix
+++ configuration/virtualization.nix
@@ -1,12 +1,5 @@
 { pkgs, ... }:
 {
-  dconf.settings = {
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
-    };
-  };
-  
   virtualisation = {
     libvirtd.enable = true;
     programs.virt-manager.enable = true;
