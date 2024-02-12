9883145d94444bfb8ebfb9bc3a7f0e4577e32baf
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-02-12 13:56:06 -0500

    syntax

--- configuration/virtualization.nix
+++ configuration/virtualization.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
+  programs.virt-manager.enable = true;
   virtualisation = {
     libvirtd.enable = true;
-    programs.virt-manager.enable = true;
     anbox.enable = true;
     podman.enable = true;
     podman.dockerSocket.enable = true;
