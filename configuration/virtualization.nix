89d9ebbc49895f2cf4aec4a39cc1b85ee0532aa2
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-02-12 13:50:54 -0500

    add anbox

--- configuration/virtualization.nix
+++ configuration/virtualization.nix
@@ -2,6 +2,14 @@
 {
   virtualisation = {
     libvirtd.enable = true;
+    programs.virt-manager.enable = true;
+    dconf.settings = {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = ["qemu:///system"];
+        uris = ["qemu:///system"];
+      };
+    };
+    virtualisation.anbox.enable = true;
     podman.enable = true;
     podman.dockerSocket.enable = true;
     podman.dockerCompat = true;
