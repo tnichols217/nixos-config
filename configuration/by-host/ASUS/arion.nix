db7f75b07f29e7477e0c9f4d243e2acdda281cc1
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-02-15 00:43:37 -0500

    update minecraft

--- configuration/by-host/ASUS/arion.nix
+++ configuration/by-host/ASUS/arion.nix
@@ -1,8 +1,12 @@
 { pkgs, attrs, config, ... }:
 {
   nixpkgs.overlays = [
-    attrs.minecraft-arion.overlays.x86_64-linux
+    # attrs.minecraft-arion.overlays.x86_64-linux
   ];
+
+  environment.variables = {
+    DOCKER_HOST = "unix:///run/podman/podman.sock";
+  };
 
   environment.systemPackages = [
     pkgs.arion
