40ec8ea8affe931960c9904190bc0ab8b37aca6d
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-02-28 11:05:03 -0500

    add swtpm

--- configuration/virtualization.nix
+++ configuration/virtualization.nix
@@ -3,12 +3,10 @@
   programs.virt-manager.enable = true;
   virtualisation = {
     libvirtd.enable = true;
+    tpm.enable = true;
     waydroid.enable = true;
     podman.enable = true;
     podman.dockerSocket.enable = true;
     podman.dockerCompat = true;
   };
-  environment.systemPackages = [
-    swtpm
-  ];
 }
