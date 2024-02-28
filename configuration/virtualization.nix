f91edcc7359a6928d084305d596546fc438cab3c
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-02-28 11:11:03 -0500

    fix typo

--- configuration/virtualization.nix
+++ configuration/virtualization.nix
@@ -1,13 +1,14 @@
 { pkgs, ... }:
 {
-  security.tpm2.enable = true;
   programs.virt-manager.enable = true;
   virtualisation = {
     libvirtd.enable = true;
-    tpm.enable = true;
     waydroid.enable = true;
     podman.enable = true;
     podman.dockerSocket.enable = true;
     podman.dockerCompat = true;
   };
+  environment.systemPackages = [
+    swtpm
+  ];
 }
