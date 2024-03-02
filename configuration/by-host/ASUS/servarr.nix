45a5974a959841b310307c0c37928f6e4b20d815
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-03-02 01:03:09 -0500

    add flaresolverr

--- configuration/by-host/ASUS/servarr.nix
+++ configuration/by-host/ASUS/servarr.nix
@@ -21,6 +21,19 @@ in
       enable = true;
     };
   };
+
+  virtualisation.oci-containers.containers = {
+    flaresolverr = {
+      imageFile = pkgs.dockerTools.pullImage{
+        imageName = "flaresolverr/flaresolverr";
+        finalImageTag = "v3.3.16";
+        imageDigest = "sha256:35feb16defbc4a0012143ec14e7eabdf361a1a1f31075db49e85ccd8cc1ee485";
+        sha256 = "000rsk86w398v9gdf1r2vy1b9190kimqmlnifsnj16nxk0bma1f4";
+      };
+    };
+  };
+
+
   users.users."${un}".extraGroups = [ "${group}" ];
 
 }
