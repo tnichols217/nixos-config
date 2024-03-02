7ceca27d9d171059a3f9379777ac65dfb446b77f
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-03-02 01:05:12 -0500

    add flaresolverr

--- configuration/by-host/ASUS/servarr.nix
+++ configuration/by-host/ASUS/servarr.nix
@@ -24,12 +24,16 @@ in
 
   virtualisation.oci-containers.containers = {
     flaresolverr = {
+      ports = [
+        "8191:8191"
+      ];
       imageFile = pkgs.dockerTools.pullImage{
         imageName = "flaresolverr/flaresolverr";
         finalImageTag = "v3.3.16";
         imageDigest = "sha256:35feb16defbc4a0012143ec14e7eabdf361a1a1f31075db49e85ccd8cc1ee485";
         sha256 = "000rsk86w398v9gdf1r2vy1b9190kimqmlnifsnj16nxk0bma1f4";
       };
+      image = "flaresolverr/flaresolverr";
     };
   };
 
