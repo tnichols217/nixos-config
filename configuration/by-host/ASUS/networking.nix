d7769e44b2c2a663e3a5b8cf39bd3367af3e6cb8
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-03-05 23:28:49 -0500

    try this?

--- configuration/by-host/ASUS/networking.nix
+++ configuration/by-host/ASUS/networking.nix
@@ -53,17 +53,17 @@
       };
     };
     networks = {
-      wg0 = {
+      "10-wg0" = {
         matchConfig.Name = "wg0";
         networkConfig.Bridge = "brwg";
-        linkConfig.RequiredForOnline = "no";
+        linkConfig.RequiredForOnline = "yes";
         address = ["10.67.135.222/32" "fc00:bbbb:bbbb:bb01::4:87dd/128"];
+        dns = ["10.64.0.1"];
         networkConfig = {
           IPMasquerade = "ipv4";
           IPForward = true;
         };
         DHCP = "no";
-        dns = ["10.64.0.1"];
       };
       "40-br0" = {
         matchConfig.Name ="brwg";
