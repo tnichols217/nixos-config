1741a865ff7f75da6d87844932801322ab08a870
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-03-11 10:27:30 -0400

    static veths

--- configuration/by-host/ASUS/servarr.nix
+++ configuration/by-host/ASUS/servarr.nix
@@ -5,7 +5,7 @@ let
   group = "servarr";
   gid = 1000;
   confCont = { name }: {
-    interfaces."ve-${name}".ipv4.addresses = [{
+    networking.interfaces."ve-${name}".ipv4.addresses = [{
       address = hostAddress name;
       prefixLength = 24;
     }];
