8ef7e0ceb09c8d946a5c79c9e4fe25a34befa291
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-04-08 18:50:46 -0400

    try abstracting 2dworkspace

--- configuration/users/templates/default_home_manager/hyprland/workspace2d.sh
+++ configuration/users/templates/default_home_manager/hyprland/workspace2d.sh
@@ -17,6 +17,27 @@ function y_value {
 	echo $(($1 / $matrix_size))
 }
 
+function moveWorkspace {
+    direction=$1
+    screen=$2
+    x=$3
+    y=$4
+    case "$direction" in
+        "left" | "move_left") x=$((($x + $matrix_size - 1) % $matrix_size));  ;;
+        "right" | "move_right") x=$((($x + 1) % $matrix_size)) ;;
+        "up" | "move_up") y=$((($y + $matrix_size - 1) % $matrix_size)) ;;
+        "down" | "move_down") y=$((($y + 1) % $matrix_size)) ;;
+        "query") echo "($x,$y)"; exit ;;
+    esac
+
+    ws=$(($max_screens * ($y * $matrix_size + $x) + $active_monitor + 1))
+
+    case "$direction" in
+        "left" | "right" | "up" | "down") hyprctl dispatch workspace $ws ;;
+        "move_left" | "move_right" | "move_up" | "move_down") hyprctl dispatch movetoworkspace $ws ;;
+    esac
+}
+
 ## Get active workspace and translate to x / y
 active_ws=$(hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id')
 active_ws=$((($active_ws  - 1) / $max_screens))
@@ -25,20 +46,22 @@ active_monitor=$(hyprctl monitors -j | jq '.[] | select(.focused) | .id')
 x=$(x_value $active_ws)
 y=$(y_value $active_ws)
 
-case "$1" in
-	"left" | "move_left") x=$((($x + $matrix_size - 1) % $matrix_size));  ;;
-	"right" | "move_right") x=$((($x + 1) % $matrix_size)) ;;
-	"up" | "move_up") y=$((($y + $matrix_size - 1) % $matrix_size)) ;;
-	"down" | "move_down") y=$((($y + 1) % $matrix_size)) ;;
-	"query") echo "($x,$y)"; exit ;;
-esac
+moveWorkspace $1 $active_monitor $x $y
 
-## Generate new workspace number
-ws=$(($max_screens * ($y * $matrix_size + $x) + $active_monitor + 1))
+# case "$1" in
+# 	"left" | "move_left") x=$((($x + $matrix_size - 1) % $matrix_size));  ;;
+# 	"right" | "move_right") x=$((($x + 1) % $matrix_size)) ;;
+# 	"up" | "move_up") y=$((($y + $matrix_size - 1) % $matrix_size)) ;;
+# 	"down" | "move_down") y=$((($y + 1) % $matrix_size)) ;;
+# 	"query") echo "($x,$y)"; exit ;;
+# esac
 
-case "$1" in
-	"left" | "right" | "up" | "down") hyprctl dispatch workspace $ws ;;
-	"move_left" | "move_right" | "move_up" | "move_down") hyprctl dispatch movetoworkspace $ws ;;
-esac
+# ## Generate new workspace number
+# ws=$(($max_screens * ($y * $matrix_size + $x) + $active_monitor + 1))
+
+# case "$1" in
+# 	"left" | "right" | "up" | "down") hyprctl dispatch workspace $ws ;;
+# 	"move_left" | "move_right" | "move_up" | "move_down") hyprctl dispatch movetoworkspace $ws ;;
+# esac
 
 reload_waybar
