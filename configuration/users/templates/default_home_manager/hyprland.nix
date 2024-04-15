a921bc1f9ddfa45a6f0e804b09f3131562555d07
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-04-15 02:29:23 -0400

    tofi

--- configuration/users/templates/default_home_manager/hyprland.nix
+++ configuration/users/templates/default_home_manager/hyprland.nix
@@ -185,8 +185,8 @@ in
             dim_around = 0.4;
             blur = {
               enabled = true;
-              size = 10;
-              passes = 3;
+              size = 3;
+              passes = 10;
               ignore_opacity = false;
               xray = false;
               noise = 0.00;
