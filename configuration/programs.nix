41d876129ffb08198e07f4f686978e46683d3bc2
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-04-23 08:16:48 -0400

    add mimetypes

--- configuration/programs.nix
+++ configuration/programs.nix
@@ -2,5 +2,15 @@
 {
   services.flatpak.enable = true;
   programs.gamemode.enable = true;
-
+  xdg.mime = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = "okular.desktop";
+      "image/png" = "gwenview.desktop";
+      "text/plain" = "codium.desktop";
+      "image/svg+xml" = "inkscape.desktop";
+      "video/mp4" = "mpv.desktop";
+      "video/mpeg" = "mpv.desktop";
+    };
+  };
 }
