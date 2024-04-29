87a2e1163e10aeda80e648ea1f9aab39025aefda
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-04-29 00:59:27 -0400

    remove nixvim

--- configuration/users/templates/default_home_manager/neovim.nix
+++ configuration/users/templates/default_home_manager/neovim.nix
@@ -4,32 +4,32 @@
     programs.neovim = {
       enable = true;
     };
-    programs.nixvim = {
-      plugins = {
-        chadtree = {
-          enable = true;
+    # programs.nixvim = {
+    #   plugins = {
+    #     chadtree = {
+    #       enable = true;
 
-        };
-        gitsigns = {
-          enable = true;
-        };
-        cmp = {
-          enable = true;
-        };
+    #     };
+    #     gitsigns = {
+    #       enable = true;
+    #     };
+    #     cmp = {
+    #       enable = true;
+    #     };
 
-        treesitter.enable = true;
-        todo-comments.enable = true;
-        nvim-autopairs.enable = true;
-        indent-blankline.enable = true;
+    #     treesitter.enable = true;
+    #     todo-comments.enable = true;
+    #     nvim-autopairs.enable = true;
+    #     indent-blankline.enable = true;
 
-        cmp-tabnine.enable = true;
-        cmp-path.enable = true;
-        cmp-cmdline.enable = true;
-        cmp-buffer.enable = true;
+    #     cmp-tabnine.enable = true;
+    #     cmp-path.enable = true;
+    #     cmp-cmdline.enable = true;
+    #     cmp-buffer.enable = true;
 
 
-        cmp-nvim-lsp.enable = true;
-      };
-    };
+    #     cmp-nvim-lsp.enable = true;
+    #   };
+    # };
   };
 }
