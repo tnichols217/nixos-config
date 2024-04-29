d660249fd233e933f7ac8cf4a34b019731e25394
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-04-29 15:40:53 -0400

    swap out base64

--- configuration/users/templates/default_home_manager/hyprland/waybar/networking.nix
+++ configuration/users/templates/default_home_manager/hyprland/waybar/networking.nix
@@ -4,7 +4,7 @@ NMCLI_COMMAND=${pkgs.networkmanager + "/bin/nmcli"}
 ROFI_COMMAND=${pkgs.tofi + "/bin/tofi"}
 NOTI_COMMAND=${pkgs.libnotify + "/bin/notify-send"}
 JQ_COMMAND=${pkgs.jq + "/bin/jq"}
-BASE64_COMMAND=${pkgs.toybox + "/bin/base64"}
+BASE64_COMMAND=${pkgs.coreutils + "/bin/base64"}
 GREP_COMMAND=${pkgs.gnugrep + "/bin/grep"}
 ${builtins.readFile ./networking.sh}
 ''
