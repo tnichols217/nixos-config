66948bbc5609e5ba6001ac33d92fba7ea05a48e5
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-05-01 15:10:08 -0400

    try diff waybar config

--- configuration/users/templates/default_home_manager/hyprland/waybar.nix
+++ configuration/users/templates/default_home_manager/hyprland/waybar.nix
@@ -1,238 +1,233 @@
 { pkgs, ... }:
 builtins.toJSON {
-    "layer" = "top"; # Waybar at top layer
-    # "position" = "bottom"; # Waybar position (top|bottom|left|right)
-    "height" = 30; # Waybar height (to be removed for auto height)
-    "spacing" = 4; # Gaps between modules (4px)
-    "start_hidden" = true;
-    "modules-right" = [
-      # "mpris"
-      "pulseaudio"
-      "keyboard-state"
-      "clock"
-      "tray"
+  "layer" = "top";
+  "position" = "top";
+  "height" = 30;
+  "spacing" = 4;
+  "start_hidden" = true;
+
+  ############################# RIGHT
+  "modules-right" = [
+    "clock"
+    "tray"
+  ];
+
+  "clock" = {
+    # "timezone" = "America/New_York";
+    "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+    "format" = "{:%Y-%m-%dT%T%Ez}";
+    "interval" = 1;
+    "format-alt" = "{:%b %a %T}";
+  };
+  "tray" = {
+    # "icon-size" = 21;
+    "spacing" = 10;
+  };
+
+  ############################# LEFT
+  "modules-left" = [
+    "hyprland/workspaces"
+    "idle_inhibitor"
+    "privacy"
+    "pulseaudio"
+    "keyboard-state"
+  ];
+
+  "hyprland/workspaces" = {
+    "format" = "{name}";
+  };
+  "idle_inhibitor" = {
+    "format" = "{icon}";
+    "format-icons" = {
+      "activated" = "";
+      "deactivated" = "";
+    };
+  };
+  "privacy" = {
+    "icon-spacing" = 4;
+    "icon-size" = 18;
+    "transition-duration" = 250;
+    "modules" = [
+      {
+        "type" = "screenshare";
+        "tooltip" = true;
+        "tooltip-icon-size" = 24;
+      }
+      {
+        "type" = "audio-out";
+        "tooltip" = true;
+        "tooltip-icon-size" = 24;
+      }
+      {
+        "type" = "audio-in";
+        "tooltip" = true;
+        "tooltip-icon-size" = 24;
+      }
     ];
-    "modules-center" = [
+  };
+  "pulseaudio" = {
+    # "scroll-step" = 1; # %; can be a float
+    "format" = "{volume}% {icon} {format_source}";
+    "format-bluetooth" = "{volume}% {icon} {format_source}";
+    "format-bluetooth-muted" = " {icon} {format_source}";
+    "format-muted" = " {format_source}";
+    "format-source" = "{volume}% ";
+    "format-source-muted" = "";
+    "format-icons" = {
+      "headphone" = "";
+      "hands-free" = "";
+      "headset" = "";
+      "phone" = "";
+      "portable" = "";
+      "car" = "";
+      "default" = ["" "" ""];
+    };
+    "on-click" = "${pkgs.pavucontrol}/bin/pavucontrol";
+    "on-click-right" = "${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+    "on-scroll-up" = "${pkgs.wireplumber}/bin/wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%+";
+    "on-scroll-down" = "${pkgs.wireplumber}/bin/wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%-";
+  };
+  "keyboard-state" = {
+    "numlock" = true;
+    "capslock" = true;
+    "format" = "{name} {icon}";
+    "format-icons" = {
+      "locked" = "";
+      "unlocked" = "";
+    };
+  };
+
+  ############################# CENTER
+  "modules-center" = [
+    "network"
+    "power-profiles-daemon"
+    "group/group-monitor"
+    "group/group-power"
+  ];
+
+  "network" = {
+    "format-wifi" = "{essid} ({signalStrength}%) ";
+    "format-ethernet" = "{ipaddr}/{cidr} 󰈀";
+    "tooltip-format" = "{ifname} via {gwaddr} ";
+    "format-linked" = "{ifname} (No IP) ";
+    "format-disconnected" = "Disconnected ⚠";
+    "format-alt" = "{ifname} = {ipaddr}/{cidr}";
+    "on-click-right" = "bash ${(pkgs.callPackage ./waybar/networking.nix {}) + "/bin/networking.sh"} connect_new";
+    "on-click-middle" = "bash ${(pkgs.callPackage ./waybar/networking.nix {}) + "/bin/networking.sh"} disconnect";
+  };
+  "power-profiles-daemon" = {
+    "format" = "{icon}";
+    "tooltip-format" = "Power profile = {profile}\nDriver = {driver}";
+    "tooltip" = true;
+    "format-icons" = {
+      "default" = "";
+      "performance" = "";
+      "balanced" = "";
+      "power-saver" = "";
+    };
+  };
+
+  "group/group-monitor" = {
+    "orientation" = "inherit";
+    "modules" = [
       "battery"
       "backlight"
-      "network"
       "cpu"
       "memory"
       "temperature"
-      "power-profiles-daemon"
-      "group/group-power"
+    ];
+    "drawer" = {
+      "transition-duration" = 500;
+      "children-class" = "not-monitor";
+      "transition-left-to-right" = true;
+    };
+  };
+  "battery" = {
+    "states" = {
+      # "good" = 95;
+      "warning" = 30;
+      "critical" = 15;
+    };
+    "format" = "{capacity}% {icon}";
+    "format-full" = "{capacity}% {icon}";
+    "format-charging" = "{capacity}% ⚡";
+    "format-plugged" = "{capacity}% ";
+    "format-alt" = "{time} {icon}";
+    # "format-good" = ""; # An empty format will hide the module
+    # "format-full" = "";
+    "format-icons" = ["" "" "" "" ""];
+  };
+  "backlight" = {
+    "format" = "{percent}% {icon}";
+    "format-icons" = ["" "" "" "" "" "" "" "" ""];
+    "on-scroll-up" = "${pkgs.brightnessctl}/bin/brightnessctl -d $(${pkgs.brightnessctl}/bin/brightnessctl -l -c backlight -m | cut -f 1 -d \",\") s 1%+";
+    "on-scroll-down" = "${pkgs.brightnessctl}/bin/brightnessctl -d $(${pkgs.brightnessctl}/bin/brightnessctl -l -c backlight -m | cut -f 1 -d \",\") s 1%-";
+  };
+  "cpu" = {
+    "interval" = 1;
+    "format" = "{usage}% ";
+    "tooltip" = false;
+  };
+  "memory" = {
+      "format" = "{}% ";
+  };
+  "temperature" = {
+    "interval" = 1;
+    # "thermal-zone" = 2;
+    # "hwmon-path" = "/sys/class/hwmon/hwmon2/temp1_input";
+    "critical-threshold" = 80;
+    # "format-critical" = "{temperatureC}°C {icon}";
+    "format" = "{temperatureC}°C {icon}";
+    "format-icons" = ["" "" ""];
+  };
 
+  "group/group-power" = {
+    "orientation" = "inherit";
+    "modules"= [
+      "custom/power"
+      "custom/quit"
+      "custom/lock"
+      "custom/reboot"
     ];
-    "modules-left" = [
-      "hyprland/workspaces"
-      "idle_inhibitor"
-      "privacy"
-    ];
-    "keyboard-state" = {
-      "numlock" = true;
-      "capslock" = true;
-      "format" = "{name} {icon}";
-      "format-icons" = {
-        "locked" = "";
-        "unlocked" = "";
-      };
+  };
+  "custom/quit"= {
+    "format"= "󰗼";
+    "tooltip"= false;
+    "on-click"= "hyprctl dispatch exit";
+  };
+  "custom/lock"= {
+    "format"= "󰍁";
+    "tooltip"= false;
+    "on-click"= "hyprlock";
+  };
+  "custom/reboot"= {
+    "format"= "󰜉";
+    "tooltip"= false;
+    "on-click"= "reboot";
+  };
+  "custom/power"= {
+    "format"= "";
+    "tooltip"= false;
+    "on-click"= "poweroff";
+  };
+
+
+
+
+
+
+
+  "mpris" = {
+    "player" = "${pkgs.playerctl}/bin/playerctl";
+    "format" = "{player_icon} {dynamic}";
+    "format-paused" = "{status_icon} <i>{dynamic}</i>";
+    "player-icons" = {
+      "default" = "▶";
+      "mpv" = "🎵";
     };
-    "mpd" = {
-      "format" = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
-      "format-disconnected" = "Disconnected ";
-      "format-stopped" = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
-      "unknown-tag" = "N/A";
-      "interval" = 5;
-      "consume-icons" = {
-        "on" = " ";
-      };
-      "random-icons" = {
-        "off" = "<span color=\"#f53c3c\"></span> ";
-        "on" = " ";
-      };
-      "repeat-icons" = {
-        "on" = " ";
-      };
-      "single-icons" = {
-        "on" = "1 ";
-      };
-      "state-icons" = {
-        "paused" = "";
-        "playing" = "";
-      };
-      "tooltip-format" = "MPD (connected)";
-      "tooltip-format-disconnected" = "MPD (disconnected)";
+    "status-icons" = {
+      "paused" = "⏸";
     };
-    "mpris" = {
-      "player" = "${pkgs.playerctl}/bin/playerctl";
-      "format" = "{player_icon} {dynamic}";
-      "format-paused" = "{status_icon} <i>{dynamic}</i>";
-      "player-icons" = {
-        "default" = "▶";
-        "mpv" = "🎵";
-      };
-      "status-icons" = {
-        "paused" = "⏸";
-      };
-      # "ignored-players": ["firefox"]
-    };
-    "privacy" = {
-      "icon-spacing" = 4;
-      "icon-size" = 18;
-      "transition-duration" = 250;
-      "modules" = [
-        {
-          "type" = "screenshare";
-          "tooltip" = true;
-          "tooltip-icon-size" = 24;
-        }
-        {
-          "type" = "audio-out";
-          "tooltip" = true;
-          "tooltip-icon-size" = 24;
-        }
-        {
-          "type" = "audio-in";
-          "tooltip" = true;
-          "tooltip-icon-size" = 24;
-        }
-      ];
-    };
-    "hyprland/workspaces" = {
-      "format" = "{name}";
-    };
-    "idle_inhibitor" = {
-      "format" = "{icon}";
-      "format-icons" = {
-        "activated" = "";
-        "deactivated" = "";
-      };
-    };
-    "tray" = {
-      # "icon-size" = 21;
-      "spacing" = 10;
-    };
-    "clock" = {
-      # "timezone" = "America/New_York";
-      "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-      "format" = "{:%Y-%m-%dT%T%Ez}";
-      "interval" = 1;
-      "format-alt" = "{:%b %a %T}";
-    };
-    "cpu" = {
-      "interval" = 1;
-      "format" = "{usage}% ";
-      "tooltip" = false;
-    };
-    "memory" = {
-        "format" = "{}% ";
-    };
-    "temperature" = {
-      "interval" = 1;
-      # "thermal-zone" = 2;
-      # "hwmon-path" = "/sys/class/hwmon/hwmon2/temp1_input";
-      "critical-threshold" = 80;
-      # "format-critical" = "{temperatureC}°C {icon}";
-      "format" = "{temperatureC}°C {icon}";
-      "format-icons" = ["" "" ""];
-    };
-    "backlight" = {
-      "format" = "{percent}% {icon}";
-      "format-icons" = ["" "" "" "" "" "" "" "" ""];
-      "on-scroll-up" = "${pkgs.brightnessctl}/bin/brightnessctl -d $(${pkgs.brightnessctl}/bin/brightnessctl -l -c backlight -m | cut -f 1 -d \",\") s 1%+";
-      "on-scroll-down" = "${pkgs.brightnessctl}/bin/brightnessctl -d $(${pkgs.brightnessctl}/bin/brightnessctl -l -c backlight -m | cut -f 1 -d \",\") s 1%-";
-    };
-    "battery" = {
-      "states" = {
-        # "good" = 95;
-        "warning" = 30;
-        "critical" = 15;
-      };
-      "format" = "{capacity}% {icon}";
-      "format-full" = "{capacity}% {icon}";
-      "format-charging" = "{capacity}% ⚡";
-      "format-plugged" = "{capacity}% ";
-      "format-alt" = "{time} {icon}";
-      # "format-good" = ""; # An empty format will hide the module
-      # "format-full" = "";
-      "format-icons" = ["" "" "" "" ""];
-    };
-    "power-profiles-daemon" = {
-      "format" = "{icon}";
-      "tooltip-format" = "Power profile = {profile}\nDriver = {driver}";
-      "tooltip" = true;
-      "format-icons" = {
-        "default" = "";
-        "performance" = "";
-        "balanced" = "";
-        "power-saver" = "";
-      };
-    };
-    "network" = {
-      # "interface" = "wlp2*"; # (Optional) To force the use of this interface
-      "format-wifi" = "{essid} ({signalStrength}%) ";
-      "format-ethernet" = "{ipaddr}/{cidr} 󰈀";
-      "tooltip-format" = "{ifname} via {gwaddr} ";
-      "format-linked" = "{ifname} (No IP) ";
-      "format-disconnected" = "Disconnected ⚠";
-      "format-alt" = "{ifname} = {ipaddr}/{cidr}";
-      "on-click-right" = "bash ${(pkgs.callPackage ./waybar/networking.nix {}) + "/bin/networking.sh"} connect_new";
-      "on-click-middle" = "bash ${(pkgs.callPackage ./waybar/networking.nix {}) + "/bin/networking.sh"} disconnect";
-    };
-    "pulseaudio" = {
-      # "scroll-step" = 1; # %; can be a float
-      "format" = "{volume}% {icon} {format_source}";
-      "format-bluetooth" = "{volume}% {icon} {format_source}";
-      "format-bluetooth-muted" = " {icon} {format_source}";
-      "format-muted" = " {format_source}";
-      "format-source" = "{volume}% ";
-      "format-source-muted" = "";
-      "format-icons" = {
-        "headphone" = "";
-        "hands-free" = "";
-        "headset" = "";
-        "phone" = "";
-        "portable" = "";
-        "car" = "";
-        "default" = ["" "" ""];
-      };
-      "on-click" = "${pkgs.pavucontrol}/bin/pavucontrol";
-      "on-click-right" = "${pkgs.wireplumber}/bin/wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-      "on-scroll-up" = "${pkgs.wireplumber}/bin/wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%+";
-      "on-scroll-down" = "${pkgs.wireplumber}/bin/wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%-";
-    };
-    "group/group-power" = {
-      "orientation" = "inherit";
-      # "drawer" = {
-      #   "transition-duration" = 500;
-      #   "children-class" = "not-power";
-      #   "transition-left-to-right" = false;
-      # };
-      "modules"= [
-        "custom/power"
-        "custom/quit"
-        "custom/lock"
-        "custom/reboot"
-      ];
-    };
-    "custom/quit"= {
-      "format"= "󰗼";
-      "tooltip"= false;
-      "on-click"= "hyprctl dispatch exit";
-    };
-    "custom/lock"= {
-      "format"= "󰍁";
-      "tooltip"= false;
-      "on-click"= "hyprlock";
-    };
-    "custom/reboot"= {
-      "format"= "󰜉";
-      "tooltip"= false;
-      "on-click"= "reboot";
-    };
-    "custom/power"= {
-      "format"= "";
-      "tooltip"= false;
-      "on-click"= "poweroff";
-    };
+    # "ignored-players": ["firefox"]
+  };
 }
