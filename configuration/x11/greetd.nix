ad804d524d9f317ef5e5db0246a4b00c81845408
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-05-01 16:45:27 -0400

    tuigreet fix

--- configuration/x11/greetd.nix
+++ configuration/x11/greetd.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "tuigreet --cmd Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland";
         user = "greeter";
       };
     };
