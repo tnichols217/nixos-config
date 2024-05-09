cd065e6215e53bcfc2bdf5d498938bd93bfe8bb5
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-05-09 18:42:03 -0400

    allow nginx for home assistant

--- configuration/by-host/ASUS/home-assistant.nix
+++ configuration/by-host/ASUS/home-assistant.nix
@@ -22,7 +22,7 @@
     config = {
       http = {
         use_x_forwarded_for = true;
-        trusted_proxies = [ 127.0.0.1 ];
+        trusted_proxies = [ "127.0.0.1" ];
         ip_ban_enabled = true;
         login_attempts_threshold = 5;
         server_port = ports.home-assistant;
