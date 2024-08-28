b2bffedcca642d0fcf84312e1c5cca5067633162
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-08-27 21:51:13 -0400

    add xbox one controller support

--- configuration/hardware.nix
+++ configuration/hardware.nix
@@ -20,6 +20,7 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.bluetooth.enable = true;
+  hardware.xone.enable = true;
   services.blueman.enable = true;
   services.power-profiles-daemon.enable = true;
 
