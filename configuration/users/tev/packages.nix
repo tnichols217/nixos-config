6f5d0fe79d2c6499e77f194121c8c34051021819
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-09-16 13:57:56 -0400

    add jupyter server

--- configuration/users/tev/packages.nix
+++ configuration/users/tev/packages.nix
@@ -46,33 +46,6 @@ in
   environment.sessionVariables = {
     MOZ_USE_XINPUT2 = "1";
   };
-  # users.users.jupyter.group = "jupyter";
-  # users.groups.jupyter = {};
-  # services.jupyter = {
-  #   enable = true;
-  #   password = "sha1:1b961dc713fb:88483270a63e57d18d43cf337e629539de1436ba";
-  #   kernels = {
-  #     python3 = let
-  #       env = (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [
-  #               ipykernel
-  #               pandas
-  #               scikit-learn
-  #             ]));
-  #     in {
-  #       displayName = "Python 3 for machine learning";
-  #       argv = [
-  #         "${env.interpreter}"
-  #         "-m"
-  #         "ipykernel_launcher"
-  #         "-f"
-  #         "{connection_file}"
-  #       ];
-  #       language = "python";
-  #       # logo32 = ${env.sitePackages}/ipykernel/resources/logo-32x32.png;
-  #       # logo64 = ${env.sitePackages}/ipykernel/resources/logo-64x64.png;
-  #     };
-  #   };
-  # };
   home-manager.users.${username} = {
     home = {
       packages = with pkgs; [
--- configuration/users/tev/services.nix
+++ configuration/users/tev/services.nix
@@ -22,7 +22,36 @@
     serviceConfig.User = "${username}";
     wantedBy = ["multi-user.target"];
   };
-  services.jupyter.enable = true;
   users.users.jupyter.group = "jupyter";
   users.groups.jupyter = {};
+  services.jupyter = {
+    enable = true;
+    password = "'argon2:$argon2id$v=19$m=10240,t=10,p=8$8fwdJqzs0V17f2Sh0fpzjg$z8mpfURqs6Zfl67DYdqZjdhDPgufg8yVjzRvjHamQUY'";
+    kernels = {
+      python3 = let
+        env = (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [
+                ipykernel
+                pandas
+                scikit-learn
+                pip
+                numpy
+                scipy
+                matplotlib
+                notebook
+              ]));
+      in {
+        displayName = "Python 3 for machine learning";
+        argv = [
+          "${env.interpreter}"
+          "-m"
+          "ipykernel_launcher"
+          "-f"
+          "{connection_file}"
+        ];
+        language = "python";
+        # logo32 = ${env.sitePackages}/ipykernel/resources/logo-32x32.png;
+        # logo64 = ${env.sitePackages}/ipykernel/resources/logo-64x64.png;
+      };
+    };
+  };
 }
