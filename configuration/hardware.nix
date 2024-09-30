df661dc4c085f298b366295a1beb3e7cbf643d48
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2024-09-30 01:29:18 -0400

    switch back to xone

--- configuration/hardware.nix
+++ configuration/hardware.nix
@@ -20,8 +20,8 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.bluetooth.enable = true;
-  # hardware.xone.enable = true;
-  hardware.xpadneo.enable = true;
+  hardware.xone.enable = true;
+  # hardware.xpadneo.enable = true;
   services.joycond.enable = true;
   hardware.nvidia.open = true;
   services.blueman.enable = true;
