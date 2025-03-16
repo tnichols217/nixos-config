17fac6356ff4dec6ecfd0badbdf0ac6e34b2b8bd
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2025-03-15 23:00:11 -0400

    add kvm amd

--- configuration/kernel.nix
+++ configuration/kernel.nix
@@ -5,7 +5,7 @@
       availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" "virtio_pci" "sr_mod" "virtio_blk" "usb_storage" "usbhid" "rtsx_usb_sdmmc" "btrfs" "usbip_host" "sdhci_pci" ];
       kernelModules = if host-name == "ASUS" then [ "nvidia" ] else [ "amdgpu" ];
     };
-    kernelModules = [ "kvm-intel" ];
+    kernelModules = [ "kvm-intel" "kvm-amd" ];
     extraModulePackages = with config.boot.kernelPackages; if host-name == "ASUS" then [
       nvidia_x11
     ] else [
