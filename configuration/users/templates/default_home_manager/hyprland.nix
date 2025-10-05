1eacbdbcd423141983c2fa5a359b371b5d1075a5
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2025-10-05 16:40:12 -0400

    update hyprland config

--- configuration/users/templates/default_home_manager/hyprland.nix
+++ configuration/users/templates/default_home_manager/hyprland.nix
@@ -232,7 +232,6 @@ in
           };
           animations = {
             enabled = true;
-            first_launch_animation = true;
           };
           animation = [
             "workspaces,1,1,default"
@@ -282,8 +281,8 @@ in
             };
           };
           gestures = {
-            workspace_swipe = true;
-            workspace_swipe_fingers = 3;
+            # workspace_swipe = true;
+            # workspace_swipe_fingers = 3;
             workspace_swipe_distance = 300;
             workspace_swipe_invert = true;
             workspace_swipe_min_speed_to_force = 30;
