980d9653a860d0cb53d26cacecdc7efc2e0c416c
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2025-11-05 03:56:57 -0500

    clean nixpkgs

--- configuration/by-host/ASUS/servarr.nix
+++ configuration/by-host/ASUS/servarr.nix
@@ -253,17 +253,6 @@ lib.mkMerge [
   (confContProw {
     name = "prowlarr";
   })
-  {
-    nixpkgs.
-    config = {
-      permittedInsecurePackages = [
-        "aspnetcore-runtime-6.0.36"
-        "aspnetcore-runtime-wrapped-6.0.36"
-        "dotnet-sdk-6.0.428"
-        "dotnet-sdk-wrapped-6.0.428"
-      ];
-    };
-  }
 ]
 
   # services = {
