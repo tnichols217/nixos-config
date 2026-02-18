7940fcd3300287a54c9764a47b9a2184353aae6d
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2026-02-18 03:00:46 -0500

    hyprland monitors

--- configuration/users/templates/default_home_manager/hyprland.nix
+++ configuration/users/templates/default_home_manager/hyprland.nix
@@ -9,10 +9,10 @@ let
       "DP-4, disable"
     ];
     "ROG" = [
+      "HDMI-A-1, highrr, auto, 1"
       "eDP-2, 2560x1600@120, auto-right, 1"
       # "HDMI-A-1, 2560x1440@120, 2560x0, 1"
       # "HDMI-A-1, highrr, 2560x0, 1"
-      "HDMI-A-1, highrr, auto, 1"
       # "DP-1, 2560x1440@60, 2560x1440, 1"
       "DP-1, 2560x1440@60, auto-down, 1"
     ];
