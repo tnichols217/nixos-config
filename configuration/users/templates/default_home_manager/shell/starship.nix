58c8f38b0db8924417f8f65a34098f1b5285b97e
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2026-04-14 03:08:10 -0400

    update starship

--- configuration/users/templates/default_home_manager/shell/starship.nix
+++ configuration/users/templates/default_home_manager/shell/starship.nix
@@ -7,7 +7,7 @@
         settings = {
           add_newline = false;
 
-          format = "[](fg:white)$os$hostname$username [](fg:white bg:dim bg:blue)$directory[](fg:blue bg:green)$git_branch$git_commit$git_status$git_state[](fg:green) ";
+          format = "[](fg:white)$os$hostname$username[](fg:white bg:dim bg:blue)$directory[](fg:blue bg:green)$git_branch$git_commit$git_status$git_state[](fg:green) ";
           right_format = "[](fg:black)$status[](bg:black fg:yellow)($cmd_duration$jobs[ ](bg:yellow fg:blue))[](bg:yellow fg:blue)($direnv$nix_shell$python$conda$kubernetes$terraform$aws$gcloud[ ](bg:blue fg:white))[](bg:blue fg:white)$time[](fg:white)";
 
           os = {
@@ -70,7 +70,7 @@
           username = {
             style_user = "bg:white fg:black";
             style_root = "bg:white fg:red";
-            format = "[$user]($style)";
+            format = "[$user ]($style)";
             disabled = false;
             show_always = false;
           };
