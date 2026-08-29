44ad0880a29b5d28051535a618b5b768cee4404a
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2026-08-29 01:48:55 -0400

    networkmanager

--- configuration/persistence/users.nix
+++ configuration/persistence/users.nix
@@ -17,7 +17,7 @@ _:
       systemd-oom.gid = 988;
       wpa_supplicant.gid = 987;
       nm-iodine.gid = 986;
-      networkmanager.gid = 989;
+      networkmanager.gid = 985;
     };
     users = {
       avahi.uid = 999;
